8d52a377d56dd6f5eda9d8f167ef5baa026749df
jacquikeating <j.keating@live.ca>
2024-06-23 16:41:12 -0400

    added routes
    - added routes to post and delete comments
    - added route to post a new video

--- routes/videos.js
+++ routes/videos.js
@@ -5,7 +5,6 @@ import uniqid from 'uniqid';
 
 const router = express.Router();
 
-let id = "b6f35f03-7936-409b-bd2a-446bcc5f30e7";
 
 function readData() {
     const videosFile = fs.readFileSync("../jacqui-keating-brainflix-api/data/videos.json");
@@ -15,7 +14,7 @@ function readData() {
 
 function writeData(data) {
     const stringifiedData = JSON.stringify(data);
-    fs.writeFileSync("/data/videos.json", stringifiedData);
+    fs.writeFileSync("../jacqui-keating-brainflix-api/data/videos.json", stringifiedData);
 }
 
 router.get('/videos', (req, res) => {
@@ -25,24 +24,86 @@ router.get('/videos', (req, res) => {
 });
 
 router.get('/videos/:id', (req, res) => {
+    const { id } = req.params;
     const videosData = readData();
     const detailedData = videosData.videosDetails;
     const matchingVideo = detailedData.find(video => video.id === id);
     res.send(matchingVideo);
 });
 
+router.post('/videos', (req, res) => {
+    const videosData = readData();
+    const videosBasics = videosData.videosBasics;
+    const videosDetails = videosData.videosDetails;
+    const newVideoDetails = {
+        id: uniqid(),
+        title: "Test video",
+        channel: "Test User",
+        image: "None",
+        description: "smfsfmsdfsfmssd",
+        views: 0,
+        duration: "1:00",
+        video: "Placeholder",
+        timestamp: Date.now(),
+        comments: []
+    };
+    const newVideoBasics = {
+       id: newVideoDetails.id,
+       title: newVideoDetails.title,
+       channel: newVideoDetails.channel,
+       image: newVideoDetails.image
+    };
+    videosBasics.push(newVideoBasics);
+    videosDetails.push(newVideoDetails);
+    writeData(videosData);
+    console.log(videosData);
+    res.status(201).send(`Your video was successfully published!`);
+});
+
 router.post('/videos/:id/comments', (req, res) => {
-    res.status(201).send(`Your comment has been posted.`);
+    const videosData = readData();
+    const { id } = req.params;
+    const detailedData = videosData.videosDetails;
+    const matchingVideo = detailedData.find(video => video.id === id);
+    if (!matchingVideo) {
+        return res.status(404).send(`Cannot find video with ID ${id}`);
+    }
+    const videoComments = matchingVideo.comments;
+    const newComment = {
+        id: uniqid(),
+        name: "Test User",
+        comment: "Placeholder comment body",
+        likes: 0,
+        timestamp: Date.now()
+    }
+    videoComments.push(newComment);
+    writeData(videosData);
+    res.status(201).send(`Your comment has been posted. Details:
+        - ID: ${newComment.id}
+        - Name: ${newComment.name}
+        - Comment: ${newComment.comment}
+        - Likes: ${newComment.likes}
+        - Timestamp: ${newComment.timestamp} `
+    );
 });
 
 router.delete('/videos/:id/comments/:commentId', (req, res) => {
-    res.status(201).send(
-        `Comment deleted:
-        ${comment.name}
-        ${comment.comment}
-        ${comment.id}
-        ${comment.timestamp}`
-    );
+    const { id, commentId } = req.params;
+    const videosData = readData();
+    const detailedData = videosData.videosDetails;
+    const matchingVideo = detailedData.find(video => video.id === id);
+    if (!matchingVideo) {
+        return res.status(404).send(`Cannot find video with ID ${id}.`);
+    }
+    const videoComments = matchingVideo.comments;
+    const matchingComment = videoComments.find(comment => comment.id == commentId);
+    if (!matchingComment) {
+        return res.status(404).send(`Cannot find comment with ID ${commentId}.`);
+    }
+    const commentIndex = videoComments.indexOf(matchingComment);
+    videoComments.splice(commentIndex, 1);
+    writeData(videosData);
+    res.status(204);
 });
 
 export default router;
